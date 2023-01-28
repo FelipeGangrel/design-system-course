6f59703ac612cc6ad923ee4817bd9d66c0a3cf58
Felipe Gangrel <felipefrog@gmail.com>
2023-01-28 16:35:42 -0300

    😭

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -1,20 +1,20 @@
 module.exports = {
-  stories: [
-    '../src/pages/**/*.stories.mdx',
-    '../src/stories/**/*.stories.@(js|jsx|ts|tsx)',
+  "stories": [
+    "../src/pages/**/*.stories.mdx",
+    "../src/stories/**/*.stories.tsx"
   ],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
-    '@storybook/addon-a11y',
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    "@storybook/addon-a11y"
   ],
-  framework: '@storybook/react',
-  core: {
-    builder: '@storybook/builder-vite',
+  "framework": "@storybook/react",
+  "core": {
+    "builder": "@storybook/builder-vite"
   },
-  features: {
-    storyStoreV7: true,
+  "features": {
+    "storyStoreV7": true
   },
   viteFinal: (config, { configType }) => {
     if (configType === 'PRODUCTION') {
